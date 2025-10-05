cced22ce122a4e71f10abada25a5cc2c1ed0f2dc
VicMelia <al415395@uji.es>
2025-10-06 00:00:29 +0200

    healing and slowing potions

--- Assets/Resources/Scripts/Spells/FireballSpell.cs
+++ Assets/Resources/Scripts/Spells/FireballSpell.cs
@@ -19,9 +19,9 @@ public class FireballSpell : BaseSpell
     protected override void Effect(Vector3 targetPosition)
     {
         GameObject fireball = Instantiate(_fireballPrefab, targetPosition + Vector3.up * 30, Quaternion.identity);
-        fireball.transform.DOScale(1, 1.5f).From(0).SetEase(Ease.OutBack);
+        fireball.transform.DOScale(1, 1f).From(0).SetEase(Ease.OutBack);
         fireball.transform.forward = -Vector3.up;
-        fireball.transform.DOMove(targetPosition, 1.5f).SetEase(Ease.InQuad).OnComplete(() =>
+        fireball.transform.DOMove(targetPosition, 1f).SetEase(Ease.InQuad).OnComplete(() =>
         {
             List<Hittable> hittables = GameManager.Instance.GetAllEnemiesInRange(targetPosition, _explosionRadius);
             foreach (Hittable hittable in hittables)
