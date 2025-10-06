d3532f1c16126763b1f1fb0386b19fef9e11b060
Diego Pérez <dperezrueda22@gmail.com>
2025-10-06 03:50:04 +0200

    Animacion de portal a medias

--- Assets/Resources/Scripts/GameManager.cs
+++ Assets/Resources/Scripts/GameManager.cs
@@ -1015,19 +1015,25 @@ public class GameManager : MonoBehaviour
             }
             _blood += unit.BloodReward;
             if (_blood > _maxBlood) _blood = _maxBlood;
-            GameObject portal = Instantiate(_portalPrefab, unit.transform.position, Quaternion.identity);
+            GameObject portal = Instantiate(_portalPrefab, unit.transform.position + Vector3.up * 0.01f, Quaternion.identity);
+            Debug.Log($"Portal instantiated at {portal.transform.position}");
+            Debug.Log(portal.transform.localScale.ToString());
             portal.transform.DOScale(Vector3.one, _portalAnimationDuration * 0.5f).From(Vector3.zero).SetEase(Ease.InOutCubic).OnComplete(() =>
             {
+                Debug.Log($"Portal animation complete for {unit.name}");
                 unit.transform.DOMove(unit.transform.position + Vector3.down * 10f, _portalAnimationDuration * 0.2f).SetEase(Ease.InOutCubic).onComplete = () =>
                 {
-                    Destroy(unit.gameObject);
+                    Destroy(unit.gameObject, 0.1f);
+                    Debug.Log($"{unit.name} destroyed.");
                 };
 
                 portal.transform.DOScale(Vector3.zero, _portalAnimationDuration * 0.5f).SetEase(Ease.InOutCubic).onComplete = () =>
                 {
-                    Destroy(portal);
+                    Destroy(portal, 0.1f);
+                    Debug.Log("Portal destroyed.");
                 };
             });
+            Debug.Log($"Portal animation started for {unit.name}");
             yield return new WaitForSeconds(_timeBetweenSpawns + 0.01f);
         }
         _enemyUnits.Clear();
